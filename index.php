859854480c155811c16a3a985a9e3662b0004dc7
Kingsong <eflakkont@gmail.com>
2019-06-14 14:04:34 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <body>
 
-<form action="s3-upload-via-form.php" method="post" enctype="multipart/form-data">
+<form action="upload.php" method="post" enctype="multipart/form-data">
   Dosya Secin:
   <input type="file" name="fileToUpload" id="fileToUpload">
   <input type="submit" value="Upload" name="submit">
