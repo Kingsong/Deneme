02e340e5ce5cd1b6b47deea206e1c796c44998fa
Kingsong <eflakkont@gmail.com>
2019-06-14 14:05:57 +0300

    Update upload.php

--- upload.php
+++ upload.php
@@ -47,7 +47,7 @@
 	
 	// For this, I would generate a unqiue random string for the key name. But you can do whatever.
 	$keyName = 'test_example/' . basename($_FILES["fileToUpload"]['tmp_name']);
-	$pathInS3 = 'https://s3.us-east-2.amazonaws.com/' . $bucketName . '/' . $keyName;
+	$pathInS3 = 'https://s3.eu-west-1.amazonaws.com/' . $bucketName . '/' . $keyName;
 
 	// Add it to S3
 	try {
