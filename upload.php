308c46e0424968d8e15ee73677e8201e8523abbd
Kingsong <eflakkont@gmail.com>
2019-09-12 04:32:15 +0300

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,19 +1,4 @@
 <?php
-	// This file demonstrates file upload to an S3 bucket. This is for using file upload via a
-	// file compared to just having the link. If you are doing it via link, refer to this:
-	// https://gist.github.com/keithweaver/08c1ab13b0cc47d0b8528f4bc318b49a
-	//
-	// You must setup your bucket to have the proper permissions. To learn how to do this
-	// refer to:
-	// https://github.com/keithweaver/python-aws-s3
-	// https://www.youtube.com/watch?v=v33Kl-Kx30o
-	
-	// I will be using composer to install the needed AWS packages.
-	// The PHP SDK:
-	// https://github.com/aws/aws-sdk-php
-	// https://packagist.org/packages/aws/aws-sdk-php 
-	//
-	// Run:$ composer require aws/aws-sdk-php
 	require 'vendor/autoload.php';
 	
 	use Aws\S3\S3Client;
